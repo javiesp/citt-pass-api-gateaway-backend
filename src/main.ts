85bff73a0481fca1c31b3baaea7e5e2444b27e66
javiesp <espinajaviera1312@gmail.com>
2024-06-22 13:10:55 -0400

    Deployment users fix

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { NestFactory } from '@nestjs/core';
-import { Transport, ClientProxyFactory, ClientProxy } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import * as process from 'process';
 
@@ -13,18 +12,9 @@ async function bootstrap() {
 
   app.enableCors(corsOptions);
 
-  const userClient: ClientProxy = ClientProxyFactory.create({
-    transport: Transport.TCP,
-    options: {
-      host: process.env.USERS_SERVICES || 'localhost',
-      port: parseInt(process.env.USERS_SERVICES_PORT, 10) || 3006,
-    },
-  });
-
-  // Asegúrate de guardar el cliente para su uso posterior si es necesario
-  app.setGlobalPrefix('api');
-
-  await app.listen(parseInt(process.env.PORT, 10) || 3005, '0.0.0.0');
+  const port = parseInt(process.env.PORT, 10) || 3005;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Gateway is listening on port ${port}`);
 }
 
 bootstrap();
