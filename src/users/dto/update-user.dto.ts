183eb90254fce95cb48c049e9c0faa1623be6250
javiesp <espinajaviera1312@gmail.com>
2024-10-13 15:11:30 -0300

    Actualizacion documento actualizar usuario

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -2,11 +2,9 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
-    uid_user: string;
     email: string;
     phone: number;
     name: string;
-    hashed_password: string
     run: string
     proyect_id: number;
 }
